51ae7e51a88f0dc570a8ca6036b4e2eeddcb5034
Cesar Augusto Arboleda Diosa <101777132+carboledad@users.noreply.github.com>
2022-06-21 17:28:18 -0500

    commit

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -21,31 +21,15 @@ def create_data_lake():
 
 
     """
-def create_data_lake():
-    """Cree el data lake con sus capas.
-
-    Esta función debe crear la carpeta `data_lake` en la raiz del proyecto. El data lake contiene
-    las siguientes subcarpetas:
-
-    ```
-    .
-    |
-    \___ data_lake/
-         |___ landing/
-         |___ raw/
-         |___ cleansed/
-         \___ business/
-              |___ reports/
-              |    |___ figures/
-              |___ features/
-              |___ forecasts/
-
-    ```
-
-
-    """
     # importing os module
     import os
+
+    # os.makedirs("data_lake/landing, exist_ok=True")
+    # os.makedirs("data_lake/raw, exist_ok=True")
+    # os.makedirs("data_lake/cleansed, exist_ok=True")
+    # os.makedirs("data_lake//business/reports/figures, exist_ok=True")
+    # os.makedirs("data_lake/business/features, exist_ok=True")
+    # os.makedirs("data_lake/business/forecasts, exist_ok=True")
 
     os.mkdir("./data_lake/")
     parent_dir = "data_lake/"
--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -3,8 +3,8 @@ Módulo de ingestión de datos.
 -------------------------------------------------------------------------------
 """
 
+# pip install wget
 
-pip install wget
 
 def ingest_data():
     """Ingeste los datos externos a la capa landing del data lake.
@@ -12,25 +12,36 @@ def ingest_data():
     archivos de precios de bolsa nacional en formato xls a la capa landing. La
     descarga debe realizarse usando únicamente funciones de Python.
     """
-    
-    
-    import wget
-    import os
 
-    os.chdir("data_lake/landind/")
-    for año in range (1995,2021):
-        if año in range(2016,2018):
-         wdir=("https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true".format(año))
-         wget.download(wdir)
-        else: 
-         wget.download("https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xlsx/{}.xls?raw=true".format(año))
-         wget.download(wdir)
-        
+    import pandas as pd
+    import xlwt
+
+    def descargar_archivo(ruta, file_name, extension):
+
+        for año in file_name:
+            url_rute = ruta + "/" + año + extension + "?raw=true"
+            nombre_archivo = "data_lake/landing/" + "{}{}".format(año, extension)
+            descarga = pd.read_excel(url_rute)
+            descarga.to_excel(
+                "data_lake/landing/{}{}".format(año, extension), index=None, header=True
+            )
+        return
+
+    ruta = "https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/"
+    file_name1 = [str(año) for año in range(1995, 2016)]
+    file_name2 = [str(año) for año in range(2018, 2022)]
+
+    file_name_xlsx = file_name1 + file_name2
+    descargar_archivo(ruta, file_name_xlsx, ".xlsx")
+
+    file_name_xls = ["2016", "2017"]
+    descargar_archivo(ruta, file_name_xls, ".xls")
     return
-    
+
+
 ingest_data()
-#raise NotImplementedError("Implementar esta función")
-    
+# raise NotImplementedError("Implementar esta función")
+
 if __name__ == "__main__":
     import doctest
 

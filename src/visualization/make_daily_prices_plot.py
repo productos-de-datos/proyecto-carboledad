237888784e3d3bc3364a50b0e7b62e20fd2de81f
Cesar Augusto Arboleda Diosa <101777132+carboledad@users.noreply.github.com>
2022-06-22 20:29:45 -0500

    commit_pipeline_plot

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -7,7 +7,35 @@ def make_daily_prices_plot():
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    path_file = r"data_lake/business/precios-diarios.csv"
+    datos = pd.read_csv(path_file, index_col=None, sep=",", header=0)
+
+    datos["fecha"] = pd.to_datetime(datos["fecha"])
+    datos["mes"] = ((datos["fecha"].dt.month).astype(int)).astype(str)
+    datos["año"] = ((datos["fecha"].dt.year).astype(int)).astype(str)
+    input = datos.groupby("año").mean({"precio": "precio"})
+    input.reset_index(inplace=True)
+
+    x = input.año
+    y = input.precio
+
+    plt.figure(figsize=(9, 6))
+    plt.plot(x, y, "b", label="Promedio Anual")
+    plt.title("Promedio Anual")
+    plt.xlabel("Año")
+    plt.ylabel("Precio")
+    plt.legend()
+    plt.xticks(rotation="vertical")
+    plt.savefig("data_lake/business/reports/figures/daily_prices.png")
+
+    return
+
+
+make_daily_prices_plot()
+# raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
